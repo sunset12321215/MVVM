ece199d71ee21ec46a0c2ecfbc7bb7f7ce5fd0db
kimochi <nguyenvantu.9b@gmail.com>
2020-08-16 16:13:29 +0700

    Initial Commit

---
+++ firstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firstApp
+//
+//  Created by KIMOCHI on 8/16/20.
+//  Copyright © 2020 KIMOCHI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
